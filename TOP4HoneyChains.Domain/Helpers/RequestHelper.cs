0d99bf31f95141818cf886390496b34bc7b72403
sahin <sahin@SAHIN-PC>
2024-08-19 11:18:17 +0300

    commit all

---
+++ TOP4HoneyChains.Domain/Helpers/RequestHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOP4HoneyChains.Domain.Helpers
+{
+    public static class RequestHelper
+    {
+        public static async Task PostAsync<T>(string url, string value)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.Timeout = TimeSpan.FromSeconds(3);
+
+                var httpResponse = await httpClient.PostAsync(url, new StringContent(value, Encoding.UTF8, "application/json"));
+            }
+        }
+
+        public static async Task<T> GetAsync<T>(string requestUri)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.Timeout = TimeSpan.FromMinutes(3);
+                    HttpResponseMessage httpResponseMessageawait = await httpClient.GetAsync(requestUri);
+                    return JsonConvert.DeserializeObject<T>(await httpResponseMessageawait.Content.ReadAsStringAsync());
+                }
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ TOP4HoneyChains.WebApp/Controllers/BeekeepersController.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TOP4HoneyChains.Domain.Helpers;
+using TOP4HoneyChains.Entities;
+
+namespace TOP4HoneyChains.WebApp.Controllers
+{
+    public class BeekeepersController : Controller
+    {
+        const string baseurl = @"https://beekeepersapi.intalalab.net/";
+        const string api = @"api/Beekeepers";
+
+        private string BuildApiUrl(string baseUrl, string api)
+        {
+            return baseUrl + api;
+        }
+
+        // GET: Beekeepers
+        public async Task<ActionResult> Index()
+        {
+            var beekeepers = new List<Beekeeper>();
+            beekeepers = await RequestHelper.GetAsync<List<Beekeeper>>(BuildApiUrl(baseurl, api));
+            return View(beekeepers);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Beekeeper beekeeper)
+        {
+
+            return View();
+        }
+
+    }
+}
