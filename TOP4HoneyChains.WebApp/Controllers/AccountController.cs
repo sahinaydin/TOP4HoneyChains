f7bf650edc677535c25fdde52d98985c18322365
sahin <sahin@SAHIN-PC>
2024-08-19 15:05:11 +0300

    commit

--- TOP4HoneyChains.WebApp/Controllers/AccountController.cs
+++ TOP4HoneyChains.WebApp/Controllers/AccountController.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TOP4HoneyChains.Entities;
 
 namespace TOP4HoneyChains.WebApp.Controllers
 {
     public class AccountController : Controller
     {
-
-        // GET: Account
-        public ActionResult Index()
+       
+        public ActionResult LogOn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
         }
     }
 }
